29bcc39a0bf933eefe798fa8d6e1fb1c5e7f790e
SwG Ghosh <codecrafts.cf@icloud.com>
2016-09-24 22:53:55 +0530

    Changes to make a web view app

--- Infinnovation Stock Exchange Simulator/ViewController.swift
+++ Infinnovation Stock Exchange Simulator/ViewController.swift
@@ -10,9 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var webView: UIWebView!
+    @IBOutlet weak var activity: UIActivityIndicatorView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        activity.startAnimating()
+        loadAddress()
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +25,19 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    func loadAddress() {
+        let requestURL = URL(string: "https://stockexchangesimulator.infinnovation.co")
+        let request = URLRequest(url: requestURL!)
+        webView.loadRequest(request)
+    }
+    
+    func webViewDidStartLoad(_ : UIWebView) {
+        activity.startAnimating()
+    }
+    
+    func webViewDidFinishLoad(_ : UIWebView) {
+        activity.stopAnimating()
+    }
+    
 }
 
