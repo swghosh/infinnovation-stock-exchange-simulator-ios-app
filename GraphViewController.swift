6c2a73ea3e57c6810b2b5b6aaaf7d17fdf4ea76b
SwG Ghosh <codecrafts.cf@icloud.com>
2017-04-24 23:24:30 +0530

    Added tableView inside GraphVC to display current, time as a demo instead of a graphical representation

--- GraphViewController.swift
+++ GraphViewController.swift
@@ -8,17 +8,57 @@
 
 import UIKit
 
-class GraphViewController: UIViewController {
+class GraphViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    var stock: StockItem?
+    var updates: [UpdateItem] = [UpdateItem]()
+    
+    @IBOutlet weak var updatesTableView: UITableView!
+    @IBOutlet weak var time: UILabel!
+    
+    func fetchAndSetup() {
+        // serialise the json response into StockItem array
+        let apiCall: UpdatesListAPICall = UpdatesListAPICall(urlString: "https://infisesapi-vistas.rhcloud.com/api/updateslist", apiKey: "Z9FpluAnvXADniEcz9Rcvg28U1CdNC", stock: stock!)
+        if apiCall.performApiCall() != nil {
+            updates = apiCall.getUpdatesList()
+            
+            time.text = apiCall.time!
+            
+        }
+        else {
+            let parentVC = self.parent as? FullStockViewController
+            parentVC?.displayNoInternet()
+            return
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        fetchAndSetup()
+        updatesTableView.reloadData()
+    }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cellIdentifier = "Update"
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier)
+        cell!.textLabel!.text = "\(updates[indexPath.row].time): ₹\(updates[indexPath.row].current)"
+        return cell!
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return updates.count
     }
     
 
