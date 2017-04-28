4b3faa0c638b1c4ddc6fcc7be15f822b77005304
SwG Ghosh <codecrafts.cf@icloud.com>
2017-04-28 17:20:12 +0530

    Refactored code in GraphView to make it better, made changes to GraphView colours in storyboard and changed axisOffset of graph; random values given for plotting demo graph

--- Infinnovation Stock Exchange Simulator/GraphView.swift
+++ Infinnovation Stock Exchange Simulator/GraphView.swift
@@ -12,76 +12,117 @@ import CoreGraphics
 @IBDesignable
 class GraphView: UIView {
     
+    // IB defines the colors to be used for various elements in the line graph
     @IBInspectable var graphAxisColor: UIColor = .black
     @IBInspectable var graphPointColor: UIColor = .blue
     @IBInspectable var graphLineColor: UIColor = .blue
     
-    var currents: [Int] = [0, 4, 6, 7, 8, 9, 10, 11, 11, 12, 13, 13, 14, 14, 15, 15, 16, 16, 17, 17, 18, 18, 19, 19, 20, 20, 20, 21, 21, 22]
+    // sample values whose graph is to be plotted
+    var currents: [Int] = [49, 57, 71, 18, 92, 81, 72, 54, 8, 2, 78, 93, 47, 56, 41, 10, 29, 72, 78, 51, 47, 21, 33, 81, 28, 8, 1, 58, 19, 53, 0]
+    // lowest and highest values in the actual graph used for calibrating the scale in the graph
     var lvalue: Int = 0
-    var hvalue: Int = 22
+    var hvalue: Int = 95
     
     // Only override draw() if you perform custom drawing.
     // An empty implementation adversely affects performance during animation.
     override func draw(_ rect: CGRect) {
         // Drawing code
         
-        // draw the axis for the graph
-        let xAxisLine = UIBezierPath()
-        xAxisLine.move(to: CGPoint(x: 5.0, y: rect.height - 5.0))
-        xAxisLine.addLine(to: CGPoint(x: rect.width, y: rect.height - 5.0))
-        xAxisLine.close()
+        // defines an offset to place the axes of the graph with respect to rect
+        let axisOffset: CGFloat = 20.0
+        // defines the point of origin of graph in terms of rect position
+        let origin: CGPoint = CGPoint(x: axisOffset, y: rect.height - axisOffset)
         
-        let yAxisLine = UIBezierPath()
-        yAxisLine.move(to: CGPoint(x: 5.0, y: 0.0))
-        yAxisLine.addLine(to: CGPoint(x: 5.0, y: rect.height - 5.0))
-        yAxisLine.close()
+        // draws the x and y axis of the graph with the specified offset
+        drawAxes(axisOffset: axisOffset)
         
-        graphAxisColor.setStroke()
-        graphAxisColor.setFill()
-        
-        xAxisLine.stroke()
-        
-        yAxisLine.stroke()
-        
-        makeGraph()
+        // plots a graph based on the integral values provide and highest and lowest values
+        plotGraph(originPoint: origin, borderOffset: 10.0, pointRadius: 5.0, yValues: currents, yLowest: lvalue, yHighest: hvalue)
     }
     
-    func makeGraph() {
+    func drawAxes(axisOffset: CGFloat) {
+        // refers to current rect
         let rect = self.bounds
         
-        let pointRadius: CGFloat = 5.0
-        let originOffset: CGFloat = pointRadius / 2;
+        // draw the axes for the graph
         
+        // draws the x axis line for the graph
+        let xAxisLine = UIBezierPath()
+        xAxisLine.move(to: CGPoint(x: axisOffset, y: rect.height - axisOffset))
+        xAxisLine.addLine(to: CGPoint(x: rect.width, y: rect.height - axisOffset))
+        xAxisLine.close()
+        
+        // draws the y axis line for the graph
+        let yAxisLine = UIBezierPath()
+        yAxisLine.move(to: CGPoint(x: axisOffset, y: 0.0))
+        yAxisLine.addLine(to: CGPoint(x: axisOffset, y: rect.height - axisOffset))
+        yAxisLine.close()
+        
+        // uses the color specified in IB to stroke the axes lines
+        graphAxisColor.setStroke()
+
+        xAxisLine.stroke()
+        yAxisLine.stroke()
+    }
+    
+    func plotGraph(originPoint: CGPoint, borderOffset: CGFloat, pointRadius: CGFloat, yValues values: [Int], yLowest lvalue: Int, yHighest hvalue: Int) {
+        // refers to the current rect
+        let rect = self.bounds
+        
+        // defines a constant float value to help in drawing points on screen with rects
+        let originOffsetForPoint: CGFloat = pointRadius / 2;
+        
+        // sets the colors for the line graph that is to be plotted based on colours specified in IB
         graphPointColor.setFill()
         graphLineColor.setStroke()
         
-        let xI: CGFloat = 5.0
-        let yI: CGFloat = rect.height - 5.0
+        // defines the x, y positions on rect of the origin of the graph
+        let xOrigin: CGFloat = originPoint.x
+        let yOrigin: CGFloat = originPoint.y
         
-        let xDiff: CGFloat = (rect.width - xI - 5.0)
-        let yDiff: CGFloat = (yI - 0.0 - 5.0)
+        // defines the difference in x, y from highest postion of graph to lowest position on graph along x, y respectively as per points on the rect
+        let xDiff: CGFloat = (rect.width - xOrigin - borderOffset)
+        let yDiff: CGFloat = (yOrigin - 0.0 - borderOffset)
         
-        let currDiff = CGFloat(hvalue - lvalue)
-        let diffScale = currDiff / yDiff
+        // defines the difference in highest and lowest of the actual graph values
+        let valDiff = CGFloat(hvalue - lvalue)
+        // defines a scaling calibration value useful for plotting of y points on graph, differenceInActualValuesOfGraph/differenceInYLimitOfRect
+        let yDiffScale = valDiff / yDiff
         
-        let xDiffScale = (xDiff / CGFloat(currents.count - 1))
+        // defines a scaling calibration value useful for plotting of x points on graph, provided that x incremennts by one for each consecutive value of y
+        let xDiffScale = (xDiff / CGFloat(values.count - 1))
         
+        // counter to run a loop to plot points and lines on the graph for each of the values
         var i = 0
-        while(i < currents.count) {
-            let point = CGRect(x: xI + (CGFloat(i) * xDiffScale) - originOffset, y: yI - ((CGFloat(currents[i]) - CGFloat(lvalue)) / diffScale) - originOffset, width: pointRadius, height: pointRadius)
-            
-            let pointPath = UIBezierPath(ovalIn: point)
+        while(i < values.count) {
+            // actual rect point for the graph point that is to be plotted on the graph
+            let point = CGPoint(x: xOrigin + (CGFloat(i) * xDiffScale), y: yOrigin - ((CGFloat(values[i]) - CGFloat(lvalue)) / yDiffScale))
+            // defining a rect with an origin offset at that point
+            let pointRect = CGRect(x: point.x - originOffsetForPoint, y: point.y - originOffsetForPoint, width: pointRadius, height: pointRadius)
+            // drawing a oval/circle in that rect to represent a point on the actual graph
+            let pointPath = UIBezierPath(ovalIn: pointRect)
+            // fill the oval with color
             pointPath.fill()
             
-            if(i + 1 != currents.count) {
+            // draw a line between this point and the next point on the graph that is to plotted on next iteration of the loop
+            if(i + 1 != values.count) {
+                // bezier path to draw the line
                 let line = UIBezierPath()
-                line.move(to: CGPoint(x: xI + (CGFloat(i) * xDiffScale), y: yI - ((CGFloat(currents[i]) - CGFloat(lvalue)) / diffScale)))
-                line.addLine(to: CGPoint(x: xI + (CGFloat(i + 1) * xDiffScale), y: yI - ((CGFloat(currents[i + 1]) - CGFloat(lvalue)) / diffScale)))
+                
+                // point to
+                let nextPoint = CGPoint(x: xOrigin + (CGFloat(i + 1) * xDiffScale), y: yOrigin - ((CGFloat(values[i + 1]) - CGFloat(lvalue)) / yDiffScale))
+                
+                // line to start from current defined point
+                line.move(to: point)
+                // line to be drawn upto the next defined point
+                line.addLine(to: nextPoint)
                 line.close()
                 
+                // stroke the line with color
                 line.stroke()
             }
             
+            // increment loop counter
             i = i + 1
         }
         
